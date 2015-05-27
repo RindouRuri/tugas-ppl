718aeedea76e1bf522a08f857cf86776a225c581
irwanmul <mulyawan.irwan@gmail.com>
2015-05-27 23:58:46 +0700

    Achievement Menu Kelupaan

--- app/src/main/java/c03/ppl/hidupsehat/Menu/AchievementMenu.java
+++ app/src/main/java/c03/ppl/hidupsehat/Menu/AchievementMenu.java
@@ -1,7 +1,15 @@
 package c03.ppl.hidupsehat.Menu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+import c03.ppl.hidupsehat.Achievement.Kalori;
+import c03.ppl.hidupsehat.Achievement.Olahraga;
+import c03.ppl.hidupsehat.R;
 
 /**
  * Created by wahyuoi on 20/05/15.
@@ -10,6 +18,30 @@ public class AchievementMenu extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.achievement);
+
+        ImageButton olahraga = (ImageButton) findViewById(R.id.olahraga);
+        ImageButton kalori = (ImageButton) findViewById(R.id.kalori);
+
+        olahraga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Olahraga.class);
+                Log.e(Olahraga.class.getName(), "Ke Menu Achievement Olahraga");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        kalori.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Kalori.class);
+                Log.e(Kalori.class.getName(), "Ke Menu Achievement Kalori");
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
